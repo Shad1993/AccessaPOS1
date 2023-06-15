ce870d3d00068036971d67b98dd0f4ae569e2ac8
Shad1993 <oderuthabdoolreshad@gmail.com>
2023-06-15 22:40:13 +0400

    transaton in pprocess

--- app/src/main/java/com/accessa/ibora/sales/scanner/inbuildScannerSunmiT2Mini.java
+++ app/src/main/java/com/accessa/ibora/sales/scanner/inbuildScannerSunmiT2Mini.java
@@ -1,96 +1,105 @@
 package com.accessa.ibora.sales.scanner;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.widget.Toast;
 
-        import androidx.activity.result.ActivityResult;
-        import androidx.activity.result.ActivityResultCallback;
-        import androidx.activity.result.ActivityResultLauncher;
-        import androidx.activity.result.contract.ActivityResultContracts;
-        import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
 
-        import com.accessa.ibora.R;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-        import java.util.ArrayList;
-        import java.util.HashMap;
+import com.accessa.ibora.R;
 
-public class inbuildScannerSunmiT2Mini extends AppCompatActivity  {
+import java.io.IOException;
 
-    private ActivityResultLauncher<Intent> scanLauncher;
+public class inbuildScannerSunmiT2Mini extends AppCompatActivity implements SurfaceHolder.Callback, Camera.PreviewCallback {
+
+    private Camera camera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private View scannerSquare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initialize the ActivityResultLauncher
-        scanLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                            Bundle bundle = result.getData().getExtras();
-                            ArrayList<HashMap<String, String>> scanResult = (ArrayList<HashMap<String, String>>) bundle.getSerializable("data");
-                            if (scanResult != null) {
-                                for (HashMap<String, String> hashMap : scanResult) {
+        surfaceView = findViewById(R.id.scanner_surface);
+        scannerSquare = findViewById(R.id.scanner_square);
 
-                                    String scanType = hashMap.get("TYPE");
-                                    String scanValue = hashMap.get("VALUE");
-
-                                    Log.i("sunmi", hashMap.get("TYPE")); // Scan type
-                                    Log.i("sunmi", hashMap.get("VALUE")); // Scan result
-                                    Toast.makeText(inbuildScannerSunmiT2Mini.this, "Scanned Barcode: " + scanValue, Toast.LENGTH_SHORT).show();
-
-                                }
-                            }
-                        }
-                    }
-                });
-
-        // Start scanning
-        startScanning();
+        // Check camera permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+        } else {
+            startCamera();
+        }
     }
 
-    private void startScanning() {
-        Intent intent = new Intent("com.summi.scan");
-        intent.setClassName("com.sunmi.sunmiqrcodescanner",
-                "com.sunmi.sunmiqrcodescanner.activity.ScanActivity");
-        intent.putExtra("CURRENT_PPI", 0X0003);// The current resolution
-        //The best for M1 & V1 is 800*480.
-        //PPI_1920_1080 = 0X0001;
-        //PPI_1280_720 = 0X0002;
-        //PPI_BEST = 0X0003;
-        // Add optional configurations to the intent
-        intent.putExtra("PLAY_SOUND", true);
-        intent.putExtra("PLAY_VIBRATE", false);
-        intent.putExtra("IDENTIFY_MORE_CODE", false);// Identify multiple QR codes in the screen, the default is false
-        intent.putExtra("IS_SHOW_SETTING", true);// Whether to display the setting button in the upper right corner, the default is true
-        intent.putExtra("IS_SHOW_ALBUM", true);// Whether to display the button to select a picture from the album, the default is true
-        intent.putExtra("IDENTIFY_INVERSE", true);// Allow to read inverse QR code, default: true
-        intent.putExtra("IS_EAN_8_ENABLE", true);//Allow to read EAN-8 code, default: true
-        intent.putExtra("IS_UPC_E_ENABLE", true);//Allow to read UPC-E code, default: true
-        intent.putExtra("IS_ISBN_10_ENABLE", true);//Allow to read ISBN-10 (from EAN-13) code, default: true
-        intent.putExtra("IS_CODE_11_ENABLE", true);//Allow to read CODE-11 code, default: false
-        intent.putExtra("IS_UPC_A_ENABLE", true);//Allow to read UPC-A code, default: true
-        intent.putExtra("IS_EAN_13_ENABLE", true);//Allow to read AN-13 code, default: true
-        intent.putExtra("IS_ISBN_13_ENABLE", true);//Allow to read ISBN-13 (from EAN-13) code, default: true
-        intent.putExtra("IS_INTERLEAVED_2_OF_5_ENABLE", true);//Allow to read Interleaved 2 of 5 codes, default: false
-        intent.putExtra("IS_CODE_128_ENABLE", true);//Allow to read ECode 128 code, default true
-        intent.putExtra("IS_CODABAR_ENABLE", true);//Allow to read Codabar code, default: true
-        intent.putExtra("IS_CODE_39_ENABLE", true);//Allow to read Code 39 code, default: true
-        intent.putExtra("IS_CODE_93_ENABLE", true);//Allow to read Code 93 code, default: true
-        intent.putExtra("IS_DATABAR_ENABLE", true);//Allow to read DataBar (RSS-14) code, default: true
-        intent.putExtra("IS_DATABAR_EXP_ENABLE", true);//Allow to read DataBar Expanded code, default: true
-        intent.putExtra("IS_Micro_PDF417_ENABLE", true);//Allow to read Micro PDF417 code, default: false
-        intent.putExtra("IS_MicroQR_ENABLE", true);//Allow to read Micro QR Code code, default: true
-        intent.putExtra("IS_OPEN_LIGHT", true);// Whether to display the flash, the default: true
-        intent.putExtra("SCAN_MODE", false);// Whether it is a loop mode, the default: false
-        intent.putExtra("IS_QR_CODE_ENABLE", true);// Allow to read QR code, default: true
-        intent.putExtra("IS_PDF417_ENABLE", true);// Allow to read PDF417 code, default: true
-        intent.putExtra("IS_DATA_MATRIX_ENABLE", true);// Allow to read DataMatrix code, default: true
-        intent.putExtra("IS_AZTEC_ENABLE", true);// Allow to read AZTEC code, default: true
-        scanLauncher.launch(intent);
+    private void startCamera() {
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+
+        // Set deprecated camera API
+        camera = Camera.open();
+        camera.setDisplayOrientation(90);
+
+        // Set the preview callback
+        camera.setPreviewCallback(this);
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // No need to handle surface changes in this example
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Release the camera and surface holder
+        camera.stopPreview();
+        camera.setPreviewCallback(null);
+        camera.release();
+        camera = null;
+        surfaceHolder = null;
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        // Process the camera preview frame here
+    }
+
+    // Handle camera permission result
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 0 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startCamera();
+        } else {
+            Toast.makeText(this, "Camera permission denied", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
 }
